aceaa1d0f849706e3eb836797be99c99d25b7ddc
Kostoski Stefan <kostoski.stefan90@gmail.com>
2025-02-03 10:36:12 +0100

    Refractor text rendering function

--- source/lumina_text.c
+++ source/lumina_text.c
@@ -12,10 +12,11 @@
 
 #include "lumina_text.h"
 #include "lumina_utf8.h"
+#include "lumina_math.h"
 #include "lumina_driver.h"
-#include <stdio.h>
+#include <stddef.h>
 
-void lumina_render_glyph(
+void lumina_text_glyph_render(
     const lumina_font_t *const font,
     const lumina_font_glyph_data_t *const glyph_data,
     const lumina_uint16_t x,
@@ -26,8 +27,8 @@ void lumina_render_glyph(
 {
     const lumina_uint8_t pixel_bitmask = (1 << font->bits_per_pixel) - 1;
     const lumina_uint16_t pixels_per_byte = 8.0f / font->bits_per_pixel;
-    const lumina_uint16_t data_length = (glyph_data->width * glyph_data->height * font->bits_per_pixel) >> 3;
-    const lumina_uint8_t *bitmap_start = &font->bitmap[glyph_data->bitmap_index];
+    const lumina_uint16_t glyph_bitmap_length = lumina_math_ceil((glyph_data->width * glyph_data->height * font->bits_per_pixel) / 8.0f);
+    const lumina_uint8_t *glyph_bitmap = &font->bitmap[glyph_data->bitmap_index];
     const lumina_uint8_t pixel_chunk_start = (pixels_per_byte - 1) * font->bits_per_pixel;
 
     lumina_uint16_t row = 0;
@@ -35,7 +36,7 @@ void lumina_render_glyph(
 
     const lumina_uint16_t glyph_y_offset = y + font->ascent - glyph_data->y_bearing;
 
-    for (lumina_uint16_t i = 0; i < data_length; ++i)
+    for (lumina_uint16_t i = 0; i < glyph_bitmap_length; ++i)
     {
         for (
             lumina_int8_t pixel_chunk_index = pixel_chunk_start;
@@ -43,7 +44,7 @@ void lumina_render_glyph(
             pixel_chunk_index -= font->bits_per_pixel
         )
         {
-            const lumina_uint8_t pixel = (bitmap_start[i] >> pixel_chunk_index) & pixel_bitmask;
+            const lumina_uint8_t pixel = (glyph_bitmap[i] >> pixel_chunk_index) & pixel_bitmask;
 
             if (pixel != 0)
             {
@@ -76,14 +77,38 @@ void lumina_render_glyph(
     }
 }
 
+static inline lumina_uint16_t lumina_text_get_alignment_offset(
+    const lumina_char_t *const text,
+    const lumina_font_t *const font,
+    const lumina_text_alignment_t alignment
+)
+{
+    switch (alignment)
+    {
+        case LUMINA_TEXT_ALIGNMENT_RIGHT:
+            return lumina_text_get_width(text, font);
+
+        case LUMINA_TEXT_ALIGNMENT_CENTER:
+            return lumina_text_get_width(text, font) >> 1;
+
+        default:
+            return 0;
+    }
+}
+
 lumina_uint16_t lumina_text_get_width(const lumina_char_t *text, const lumina_font_t *const font)
 {
+    if (text == NULL || text[0] == '\0' || font == NULL)
+    {
+        return 0;
+    }
+
     lumina_uint16_t width = 0;
 
     lumina_utf8_iterator_t text_iterator = lumina_utf8_iterator_create(text);
     lumina_utf8_unicode_codepoint_t codepoint;
 
-    const lumina_font_glyph_data_t *glyph_data;
+    const lumina_font_glyph_data_t *glyph_data = NULL;
 
     while ((codepoint = lumina_utf8_iterator_next(&text_iterator)) != 0)
     {
@@ -97,52 +122,44 @@ lumina_uint16_t lumina_text_get_width(const lumina_char_t *text, const lumina_fo
         width += glyph_data->advance;
     }
 
-    return width - glyph_data->advance + glyph_data->width - 1;
+    if (glyph_data == NULL)
+    {
+        return width;
+    }
+
+    return width - glyph_data->advance + glyph_data->width;
 }
 
-void lumina_render_text(
+void lumina_text_render(
     const lumina_char_t *text,
     const lumina_uint16_t x,
     const lumina_uint16_t y,
     const lumina_text_alignment_t alignment,
-    const lumina_text_style_t *const style
+    const lumina_font_t *const font,
+    const lumina_color_t foreground_color,
+    const lumina_color_t background_color
 )
 {
-    lumina_uint16_t pen_x = x;
-
-    switch (alignment)
+    if (text == NULL || text[0] == '\0' || font == NULL)
     {
-        case LUMINA_TEXT_ALIGNMENT_LEFT:
-            break;
-
-        case LUMINA_TEXT_ALIGNMENT_RIGHT:
-            {
-                const lumina_uint16_t text_width = lumina_text_get_width(text, style->font);
-                pen_x = (text_width > pen_x) ? 0 : pen_x - text_width;
-                break;
-            }
-
-        case LUMINA_TEXT_ALIGNMENT_CENTER:
-            {
-                const lumina_uint16_t half_text_width = lumina_text_get_width(text, style->font) / 2;
-                pen_x = (half_text_width > pen_x) ? 0 : pen_x - half_text_width;
-                break;
-            }
+        return;
     }
+
+    lumina_uint16_t pen_x = lumina_math_max(0,  x - lumina_text_get_alignment_offset(text, font, alignment));
 
     lumina_utf8_iterator_t text_iterator = lumina_utf8_iterator_create(text);
     lumina_utf8_unicode_codepoint_t codepoint;
 
     while ((codepoint = lumina_utf8_iterator_next(&text_iterator)) != 0)
     {
-        const lumina_font_glyph_data_t *const glyph_data = lumina_font_get_glyph_data(style->font, codepoint);
+        const lumina_font_glyph_data_t *const glyph_data = lumina_font_get_glyph_data(font, codepoint);
 
         if (glyph_data == NULL)
         {
             continue;
         }
 
-        lumina_render_glyph(style->font, glyph_data, pen_x, y, style->foreground_color, style->background_color);
+        lumina_text_glyph_render(font, glyph_data, pen_x, y, foreground_color, background_color);
 
         pen_x += glyph_data->advance;
     }
